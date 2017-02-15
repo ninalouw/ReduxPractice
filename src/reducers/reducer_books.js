db06a6210393ac0574213d3ca9df59c42ee735bb
Nina Louw <ninalouw@mac.com>
2017-02-15 11:52:29 +0200

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// func that will return an array of books
+export default function () {
+  return [
+    { title: 'JS The good Parts' },
+    { title: 'Harry Potter and the Chamber of Secrets' },
+    { title: 'Middlemarch' },
+    { title: 'The English Patient' },
+    { title: 'Wolf Hall' }
+  ];
+}
